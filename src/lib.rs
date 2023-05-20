342a44ccdab5313d01658505ec5bc156f0b9d0d8
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-05-20 14:52:18 +0200

    Put the puzzles in a cache directory once fetched

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
-use std::env;
+use std::{env, fs};
 
 use reqwest::header::{HeaderMap, COOKIE};
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub async fn get_aoc_input(day: u8) -> Result<String> {
+    if !std::path::Path::new(".cache/puzzles/").exists() {
+        fs::create_dir_all(".cache/puzzles/")?;
+    }
+
+    let cached_puzzle_path: String = format!(".cache/puzzles/day_{day}.txt");
+
+    if std::path::Path::new(&cached_puzzle_path).exists() {
+        println!("Using the cached puzzle for day {day} !");
+
+        let puzzle = fs::read_to_string(cached_puzzle_path)
+            .expect("An error occurred while reading a file that supposedly exists.");
+        return Ok(puzzle);
+    }
+
     dotenvy::dotenv()?;
 
     let session_cookie = env::var("SESSION_COOKIE").expect("Please fill the SESSION_COOKIE variable in a .env file or run the executable with: SESSION_COOKIE=\"...\" <executable>");
@@ -20,6 +34,8 @@ pub async fn get_aoc_input(day: u8) -> Result<String> {
         .await?;
 
     let body = res.text().await?;
+
+    fs::write(&cached_puzzle_path, &body).expect("Unable to write the puzzle input into the cache");
 
     Ok(body)
 }
