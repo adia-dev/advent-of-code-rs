ed48ad4fc8f97a5d9491c1e4c8f92e92678dc606
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-05-20 04:20:19 +0200

    day 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
-const URI: &'static str = "https://adventofcode.com/2022/day/1/input";
+use std::env;
 
-pub fn get_aoc_input(day: &str) -> String {
-    "".into()
+use reqwest::header::{HeaderMap, COOKIE};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub async fn get_aoc_input(day: u8) -> Result<String> {
+    dotenvy::dotenv()?;
+
+    let session_cookie = env::var("SESSION_COOKIE").expect("Please fill the SESSION_COOKIE variable in a .env file or run the executable with: SESSION_COOKIE=\"...\" <executable>");
+
+    let mut headers = HeaderMap::new();
+    headers.insert(COOKIE, session_cookie.parse().unwrap());
+    let client = reqwest::Client::new();
+
+    let res = client
+        .get(format!("https://adventofcode.com/2022/day/{}/input", day))
+        .headers(headers)
+        .send()
+        .await?;
+
+    let body = res.text().await?;
+
+    Ok(body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use advent_of_code::get_aoc_input;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // init
+    dotenvy::dotenv()?;
+
+    let input = get_aoc_input(1).await?;
+    let calories: Vec<&str> = input.split("\n").collect();
+    let mut groups: Vec<i32> = Vec::new();
+    let mut current = 0;
+
+    for c in calories.into_iter() {
+        if c.len() > 0 {
+            current += c.parse::<i32>().unwrap();
+        } else {
+            groups.push(current);
+            current = 0;
+        }
+    }
+
+    groups.sort();
+
+    let max = groups.iter().max().unwrap();
+
+    let top_three: i32 = groups.iter().rev().take(3).sum();
+
+    println!("1: {max}");
+    println!("2: {top_three}");
+
+    Ok(())
 }
